c997e0f0d0680b00f0e5e68a9eacec27d3199b8d
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-02 10:52:29 +0600

    solved hollowdiamond shape pattern

--- hollowDimond.cpp
+++ hollowDimond.cpp
@@ -7,36 +7,33 @@ int main(){
 	cin>>n;
 	
 	for(int i = 0; i<n; i++){
-		for(int j = n; j>i; j--){
+		for(int j = 0 ; j<n-i-1; j++){
 			cout<<" ";
 		}
 		cout<<"*";
-	for(int j = 0; j<i; j++){
-			cout<<" ";
-		}
-	for(int j = 0; j<i; j++){
+	if(i!=0){
+	 for(int j = 0; j<2*i-1; j++){
 			cout<<" ";
 		}
 		cout<<"*";
-		
+	}
 		cout<<"\n";
 	}
 	
-	for(int i = 0; i<n; i++){
-	  for(int j = 0; j<i; j++){
+	for(int i = 0; i<n-1; i++){
+	  for(int j = 0; j<i+1; j++){
 			cout<<" ";
 		}
 		cout<<"*";
-	for(int j = n; j>i; j--){
+	if(i!=n-2){
+	for(int j = 0; j<2*(n-i)-5; j++){
 			cout<<" ";
 		}
 	
-	for(int j = n; j>i; j--){
-			cout<<" ";
-		}
 		cout<<"*";
-		cout<<"\n";
 	}
-	
+		cout<<"\n";
+
+}
 	return 0;
 }
