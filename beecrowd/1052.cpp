af4fd45b40df0838107c0e092f8ea625a13b2841
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-07-13 04:29:42 +0600

    solving basic

---
+++ beecrowd/1052.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int m_no;
+    cin>>m_no;
+
+    if(m_no==1){
+        cout<<"January\n";
+    }
+    else if(m_no==2){
+        cout<<"February\n";
+    }
+    else if(m_no==3){
+        cout<<"March\n";
+    }
+    else if(m_no==4){
+        cout<<"April\n";
+    }
+    else if(m_no==5){
+        cout<<"May\n";
+    }
+    else if(m_no==6){
+        cout<<"June\n";
+    }
+    else if(m_no==7){
+        cout<<"July\n";
+    }
+    else if(m_no==8){
+        cout<<"August\n";
+    }
+    else if(m_no==9){
+        cout<<"September\n";
+    }
+     else if(m_no==10){
+        cout<<"October\n";
+    }
+     else if(m_no==11){
+        cout<<"November\n";
+    }
+    else cout<<"December\n";
+
+    return 0;
+}
