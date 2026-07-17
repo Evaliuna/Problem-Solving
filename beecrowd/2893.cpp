fe196d6e90f62f62f5b7d0f7aba9c7c8da49d193
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-07-17 11:04:13 +0600

    Update 2893.cpp

---
+++ beecrowd/2893.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+  
+    
+   
+        while(cin>>t){
+            double fastest; 
+            cin>>fastest;
+
+            for(int i=1; i<t; i++){
+                double ti;
+                cin>>ti;
+                if(fastest>ti) fastest = ti;
+
+            }
+            cout<<fixed<<setprecision(2)<<fastest<<"\n";           
+        }
+     
+  
+      
+
+    return 0;
+}
